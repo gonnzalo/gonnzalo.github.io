0ac273efa8f2e464c1b6e970fea2ff54d1208d05
Gonzalo <arecogonzalo@gmail.com>
2019-04-06 15:59:16 +0800

    fixed offset navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,24 +1,34 @@
 import { Link } from "gatsby"
 import React from "react"
+import AnchorLink from "react-anchor-link-smooth-scroll"
+import "./header.css"
 
 const Header = () => (
   <header>
     <div className="nav-container">
       <div className="logo">
         <h1>
-          <Link to="/">WEB</Link>
+          <Link to="/" className="logo">
+            WEB
+          </Link>
         </h1>
       </div>
       <nav className="navbar">
         <ul className="nav-list">
           <li>
-            <a href="#about">About</a>
+            <AnchorLink offset="100" href="#about" className="link-effect">
+              About
+            </AnchorLink>
           </li>
           <li>
-            <a href="#projects">Projects</a>
+            <AnchorLink offset="100" href="#projects" className="link-effect">
+              Projects
+            </AnchorLink>
           </li>
           <li>
-            <a href="#contact">Contact</a>
+            <AnchorLink offset="100" href="#contact" className="link-effect">
+              Contact
+            </AnchorLink>
           </li>
         </ul>
       </nav>
