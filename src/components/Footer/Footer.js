028b24500dd89afe9246198bc8e1d218750ac608
Gonzalo <arecogonzalo@gmail.com>
2019-04-07 19:44:50 +0800

    add hook scroll for header effects

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,4 +1,5 @@
 import React from "react"
+import "./footer.css"
 
 const Footer = () => <footer>© {new Date().getFullYear()}, footer</footer>
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,39 +1,54 @@
 import { Link } from "gatsby"
-import React from "react"
+import React, { useState, useEffect } from "react"
 import AnchorLink from "react-anchor-link-smooth-scroll"
 import "./header.css"
 
-const Header = () => (
-  <header>
-    <div className="nav-container">
-      <div className="logo">
-        <h1>
-          <Link to="/" className="logo">
-            WEB
-          </Link>
-        </h1>
-      </div>
-      <nav className="navbar">
-        <ul className="nav-list">
-          <li>
-            <AnchorLink offset="100" href="#about" className="link-effect">
-              About
-            </AnchorLink>
-          </li>
-          <li>
-            <AnchorLink offset="100" href="#projects" className="link-effect">
-              Projects
-            </AnchorLink>
-          </li>
-          <li>
-            <AnchorLink offset="100" href="#contact" className="link-effect">
-              Contact
-            </AnchorLink>
-          </li>
-        </ul>
-      </nav>
-    </div>
-  </header>
-)
+const Header = () => {
+  const [isHeaderActive, setHeaderActive] = useState(false)
 
+  useEffect(() => {
+    const handleScroll = () => {
+      return window.scrollY > 10
+        ? setHeaderActive(true)
+        : setHeaderActive(false)
+    }
+    window.addEventListener("scroll", handleScroll)
+    return () => {
+      window.removeEventListener("scroll", handleScroll)
+    }
+  })
+
+  return (
+    <header className={`${isHeaderActive ? "header-active" : ""}`}>
+      <div className="nav-container">
+        <div className="logo">
+          <h1>
+            <Link to="/" className="logo">
+              WEB
+            </Link>
+          </h1>
+        </div>
+        <nav className="navbar">
+          <ul className="nav-list">
+            <li>
+              <AnchorLink offset="100" href="#about" className="link-effect">
+                About
+              </AnchorLink>
+            </li>
+            <li>
+              <AnchorLink offset="100" href="#projects" className="link-effect">
+                Projects
+              </AnchorLink>
+            </li>
+            <li>
+              <AnchorLink offset="100" href="#contact" className="link-effect">
+                Contact
+              </AnchorLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  )
+}
 export default Header
